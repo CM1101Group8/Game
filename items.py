310fcda7030fcca9f0ead44891c64e56c33c70ee
TomDurston <t.durston@hotmail.co.uk>
2014-10-20 12:34:35 +0000

    added items

--- items.py
+++ items.py
@@ -1,63 +1,97 @@
-item_id = {
-    "id": "id",
+item_toolkit = {
+    "id": "toolkit",
 
-    "name": "id card",
-
-    "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?...""",
-
-    "mass": 20
-}
-
-item_laptop = {
-    "id": "laptop",
-
-    "name": "laptop",
+    "name": "a Multi-Toolkit",
 
     "description":
-    "It has seen better days. At least it has a WiFi card!",
+    """The Toolkit contains a knife, pliers, wire cutters. This could be useful for cutting wires which might get in your way!""",
 
-    "mass": 2000
+    "mass": 300
 }
 
-item_money = {
-    "id": "money",
+item_fireblanket = {
+    "id": "fireblanket",
 
-    "name": "money",
+    "name": "a fire blanket",
 
     "description":
-    "This wad of cash is barely enough to pay your tuition fees.",
+    """The fire blanket will shield you from flames, allowing you to access areas blocked off from fire.""",
 
-    "mass": 50
+    "mass": 250
 }
 
-item_biscuits = {
-    "id": "biscuits",
+item_headtorch = {
+    "id": "headtorch",
 
-    "name": "a pack of biscuits",
+    "name": "a headtorch",
 
-    "description": "A pack of biscuits.",
+    "description":
+    """The headtorch can be used to see in dark areas. This could allow you to explored dark places such as caves.""",
 
-    "mass": 500
+    "mass": 150
 }
 
-item_pen = {
-    "id": "pen",
+item_compass = {
+    "id": "toolkit",
+
+    "name": "a compass",
+
+    "description":
+    """The compass will allow you to navigate directions, aiding you in your attemp for rescue.""",
+
+    "mass": 250
+}
+
+item_parachute = {
+    "id": "parachute",
+
+    "name": "a parachute",
+
+    "description":
+    """The parachute is still in good condition and could be used as a shelter.""",
+
+    "mass": 1500
+}
+
+item_petrol = {
+    "id": "petrol",
+
+    "name": "a canister of petrol",
+
+    "description":
+    """A canister of petrol which could be used to aid in firemaking.""",
+
+    "mass": 1200
+}
+
+item_sparktool = {
+    "id": "sparktool",
+
+    "name": "a spark tool",
+
+    "description":
+    """The spark tool which is made out of flint can be used to create sparks. When combined with other items it could produce fire.""",
+
+    "mass": 250
+}
+
+item_greenleaves = {
+    "id": "greenleaves",
+
+    "name": "a pile of green leaves",
+
+    "description":
+    """An ordinary pile of green leaves.""",
+
+    "mass": 100
+}
+
+item_wood= {
+    "id": "wood",
     
-    "name": "a pen",
+    "name": "a pile of wood",
 
-    "description": "A basic ballpoint pen.",
+    "description": "The wood pile can be used as fuel for a fire.",
 
-    "mass": 30
-}
-
-item_handbook = {
-    "id": "handbook",
-    
-    "name": "a student handbook",
-
-    "description": "This student handbook explains everything. Seriously.",
-
-    "mass": 600
+    "mass": 1000
 }

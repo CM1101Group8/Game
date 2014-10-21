a3012c54e1c4d37c11e235d494045478cc0a15bc
savavel <savavelichkov@gmail.com>
2014-10-21 17:50:54 +0300

    removed machete

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ player = {
     "name": "Group 8",
     "health": 100,
     "experience": 100,
-    "inventory": [item_toolkit, item_fireblanket, item_headtorch, item_compass, item_machete],
+    "inventory": [item_toolkit, item_fireblanket, item_headtorch, item_compass],
     "inventory_weight": 950,
     "current_location": locations["Plane"],
     "previous_location": locations["Plane"]
